be071eec8b0bb0dd819464cb4f5c82ddb0c8ac8b
Bryce Hedelius <50183652+bhedelius@users.noreply.github.com>
2021-02-24 18:08:32 -0700

    Update anidataset.py

--- anidataset.py
+++ anidataset.py
@@ -10,12 +10,17 @@ Original file is located at
 import h5py
 import numpy as np
 
+import os
+
 import torch
 from torch.utils.data import Dataset
 
+#!pip install dgl-cu101
+import dgl
+
 # Install Ani-1x Dataset:
 #
-#wget https://s3-eu-west-1.amazonaws.com/pstorage-npg-968563215/18112775/ani1xrelease.h5
+#!wget https://s3-eu-west-1.amazonaws.com/pstorage-npg-968563215/18112775/ani1xrelease.h5
 
 def iter_data_buckets(h5filename, keys=['wb97x_dz.energy']):
     """ Iterate over buckets of data in ANI HDF5 file. 
@@ -40,41 +45,70 @@ def iter_data_buckets(h5filename, keys=['wb97x_dz.energy']):
             d['coordinates'] = grp['coordinates'][()][mask]
             yield d
 
-species_dict = {1: 0, 6: 1, 7: 2, 8: 3}
+eye = np.eye(4)
+species_dict = {1: eye[0], 6: eye[1], 7: eye[2], 8: eye[3]}
 
 class AniDataset(Dataset):
-  def __init__(self, dir='./'):
+  def __init__(self, dir='./', cutoff=100):
     super(AniDataset, self).__init__()
     self.parse(dir)
+    self.cutoff = cutoff
 
   def parse(self, dir):
     self.species = []
     self.pos = []
-    self.energy = []
     self.forces = []
 
     h5file = os.path.join(dir,'ani1xrelease.h5')
     iter = iter_data_buckets(h5file, keys=['mp2_tz.corr_energy','wb97x_tz.forces'])
     for molecule in iter:
       species = molecule['atomic_numbers']
-      for pos, energy, forces  in zip(molecule['coordinates'], molecule['mp2_tz.corr_energy'], molecule['wb97x_tz.forces']):
+      for pos, forces  in zip(molecule['coordinates'], molecule['wb97x_tz.forces']):
         self.species.append(species)
         self.pos.append(pos)
-        self.energy.append(energy)
         self.forces.append(forces)
+
+  @staticmethod
+  def get_edges(pos, cutoff):
+    dist_mat = np.linalg.norm(pos[None,:,:]-pos[:,None,:],axis=2)
+
+    u = []
+    v = []
+    N = len(pos)
+    for i in range(N):
+      for j in range(N):
+        if i!=j and dist_mat[i,j]<cutoff:
+          u.append(i)
+          v.append(j)
+    edges = torch.tensor(u, dtype=torch.long), torch.tensor(v, dtype=torch.long)
+    return edges
 
   def __getitem__(self, i):
     pos = self.pos[i]
     species = self.species[i]
-    energy = self.energy[i]
     forces = self.forces[i]
 
     pos = torch.tensor(pos)
     species = torch.tensor([species_dict[atom] for atom in species], dtype=torch.long)
-    energy = torch.tensor(energy)
-    return pos, species, energy, forces
+    forces = torch.tensor(forces)
+
+    edges = self.get_edges(pos, self.cutoff)
+
+    graph = dgl.graph(edges)
+    graph.ndata['pos'] = pos
+    graph.ndata['species'] = species
+    graph.ndata['forces'] = forces
+
+    return graph
 
   def __len__(self):
-    return len(self.energy)
+    return len(self.pos)
 
 #ani_dataset = AniDataset()
+
+#ani_dataset[4000]
+
+#species = ani_dataset.species[0]
+
+#[species_dict[atom] for atom in species]
+
