43e99aad4947c6625838f50b59be500bd6aceda4
Bryce Hedelius <50183652+bhedelius@users.noreply.github.com>
2021-04-05 12:21:33 -0600

    Update se3_transformer.py

--- forcefield/se3_transformer.py
+++ forcefield/se3_transformer.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import torch
 from torch import nn
 import numpy as np
@@ -41,10 +40,8 @@ def _basis_transformation_Q_J(J, order_in, order_out, version=3):  # pylint: dis
     return Q_J  # [m_out * m_in, m]
 
 
-
 class PointCloud:
-  '''Represents a point cloud in R^3. This class calculates and stores relevant
-  geometric information such as the vectors, distances, directions, and
+  '''Represents a point cloud in R^3. This class calculates and stores relevant           geometric information such as the vectors, distances, directions, and
   spherical harmonics of the vectors.'''
   def __init__(self, pos=None, graph=None, cutoff=8.0, J_max=4):
     if graph is None:
@@ -79,7 +76,7 @@ class PointCloud:
     self.graph.edata['r_ij'] = r_ij
     self.Y = precompute_sh(r_ij, J_max)
     self.graph.edata['dist'] = torch.norm(vec, dim=1)
-
+ 
   def get_sh(self, J):
     # Returns spherical harmonic of order J.
     if not J in self.Y.keys(): # If J <= J_max this is false.
@@ -89,7 +86,7 @@ class PointCloud:
         if not key in self.Y.keys():
           self.Y[key] = Y_new[key]
     return self.Y[J]
-    
+
   def get_w_j(self, l, k):
     # Returns basis kernel
     if not (l,k) in self.w_j.keys():
@@ -101,12 +98,13 @@ class PointCloud:
         w_j[j] = torch.matmul(Y_J,Q_J.T).reshape(num_edgs, 2*l+1, 2*k+1)
       self.w_j[(l,k)] = w_j.transpose(0,1)
     return self.w_j[(l,k)]
-  
+
   def to(self, dev):
     self.graph = self.graph.to(dev)
     for key, value in self.w_j.items():
       self.w_j[key] = value.to(dev)
-      
+    return self
+
 # Dictionary for indices
 # e: edges
 # o: c_out
@@ -116,39 +114,52 @@ class PointCloud:
 # j: hidden tensor representation
 
 class WLayer(nn.Module):
-  def __init__(self, k, l, c_in=1, c_out=1):
+  def __init__(self, k, l, c_in=1, c_out=1, d_max=1):
     super(WLayer, self).__init__()
     self.k = k
     self.l = l
     self.c_in = c_in
     self.c_out = c_out
+    self.d_max = d_max
 
     J_size = k+l-abs(k-l)+1
     self.J_size = J_size
     r_size = J_size * c_out * c_in
 
-    radial = nn.Sequential(nn.Linear(1,10),
-                           nn.BatchNorm1d(10),
-                           nn.ReLU(),
-                           nn.Linear(10,30),
-                           nn.BatchNorm1d(30),
-                           nn.ReLU(),
-                           nn.Linear(30,r_size))
-    nn.init.xavier_normal_(radial[0].weight)
-    nn.init.xavier_normal_(radial[3].weight)
-    nn.init.xavier_normal_(radial[6].weight)
-    self.radial = radial
+    self.radial = nn.Sequential(nn.Linear((1+d_max)*c_in+1, 16),
+                                nn.BatchNorm1d(16),
+                                nn.ReLU(),
+                                nn.Linear(16, 32),
+                                nn.BatchNorm1d(32),
+                                nn.ReLU(),
+                                nn.Linear(32, r_size))
+    nn.init.xavier_normal_(self.radial[0].weight)
+    nn.init.xavier_normal_(self.radial[3].weight)
+    nn.init.xavier_normal_(self.radial[6].weight)
+    
+  def prep_input(self, pc, f):
+    c_in = self.c_in
+    d_max = self.d_max
+    u, v = pc.graph.edges() 
+    input_vector = torch.zeros(pc.graph.num_edges(), 1+c_in*(1+d_max), device=f[0].device)
+    idx = 0
+    for i in range(self.d_max+1):
+      input_vector[:,idx:idx+c_in] = torch.sum(f[i][u]*f[i][v], dim=2)
+      idx += c_in
+    input_vector[:,idx] = pc.graph.edata['dist']
+    return input_vector
 
-  def forward(self, pc):
-    l, k = self.l, self.k
-    w_j = pc.get_w_j(l, k)
+  def forward(self, pc, f):
+    w_j = pc.get_w_j(self.l, self.k)
     w_j.requires_grad = False
-    dist = pc.graph.edata['dist'][:,None]
+
+    input_vector = self.prep_input(pc, f)
+
     size = (pc.graph.number_of_edges(), self.J_size, self.c_out, self.c_in)
-    R = self.radial(dist).view(*size)
+    R = self.radial(input_vector).view(*size)
     w = torch.einsum('ejoi,ejlk->eoilk', R, w_j)
     return w
-    
+
 class AttnBlock(nn.Module):
   def __init__(self, d_in, d_out, c_in=1, c_out=1):
     super(AttnBlock, self).__init__()
@@ -158,40 +169,41 @@ class AttnBlock(nn.Module):
     self.c_in = c_in
     self.c_out = c_out
 
-    wq = nn.Parameter(torch.randn(d_in+1, c_out, c_in))
-    nn.init.xavier_normal_(wq)
-    self.wq = wq
+    self.wq = nn.Parameter(torch.randn(d_in+1, c_out, c_in))
+    nn.init.xavier_normal_(self.wq)
 
     self.wk_layers = nn.ModuleList([
-                      nn.ModuleList([WLayer(k, l, c_in, c_out)
+                      nn.ModuleList([WLayer(k, l, c_in, c_out, d_max = d_in)
                         for k in range(d_in+1)])
                       for l in range(d_out+1)])
 
   def forward(self, pc, f):
-    for key, value in f.items():
-      pc.graph.ndata[key] = value
     pc.graph.ndata['q'] = self.calc_q(pc, f)
 
     for l in range(self.d_out+1):
       for k in range(self.d_in+1):
-        pc.graph.edata[(k,l)] = self.wk_layers[l][k](pc)
+        pc.graph.edata[(k,l)] = self.wk_layers[l][k](pc, f)
 
+    for key, value in f.items():
+          pc.graph.ndata[key] = value
     pc.graph.update_all(self.attn_msg, self.attn_rdc)
-    a = pc.graph.edata['exp'] / pc.graph.ndata['sum'][pc.graph.edges()[1]]
+    lse = pc.graph.ndata['lse']
+    dot = pc.graph.edata['dot']
+    a = torch.exp(dot-lse[pc.graph.edges()[1]])
     return a
 
   def attn_msg(self, edges):
     k = self.calc_k(edges)
     q = edges.dst['q']
-    exp = torch.exp(torch.einsum('eol,eol->e',q,k))
-    edges.data['exp'] = exp
-    return {'exp': exp}
+    dot = torch.einsum('eol,eol->e',q,k)
+    edges.data['dot'] = dot
+    return {'dot': dot}
 
   def attn_rdc(self, nodes):
     # does sum over j'
-    exp = nodes.mailbox['exp']
-    sum = torch.sum(exp, dim=1)
-    return {'sum': sum}
+    dot = nodes.mailbox['dot']
+    lse = torch.logsumexp(dot, dim=1)
+    return {'lse': lse}
 
   def calc_q(self, pc, f):
     ql = []
@@ -228,7 +240,7 @@ class GConv(nn.Module):
 
     self.wv_layers = nn.ModuleList([
                         nn.ModuleList([
-                          WLayer(k, l, c_in=c_in, c_out=c_out)
+                          WLayer(k, l, c_in=c_in, c_out=c_out, d_max=d_in)
                           for l in range(d_out+1)])
                         for k in range(d_in+1)])
 
@@ -239,17 +251,17 @@ class GConv(nn.Module):
       si_nets = []
       for _ in range(min(d_in, d_out)+1):
         si_net = nn.Sequential(nn.Linear(c_in**2, 2*c_in*c_out),
-                              nn.BatchNorm1d(2*c_in*c_out),
-                              nn.ReLU(),
-                              nn.Linear(2*c_in*c_out, 2*c_in*c_out),
-                              nn.BatchNorm1d(2*c_in*c_out),
-                              nn.ReLU(),
-                              nn.Linear(2*c_in*c_out, c_in*c_out))
+                               nn.BatchNorm1d(2*c_in*c_out),
+                               nn.ReLU(),
+                               nn.Linear(2*c_in*c_out, 2*c_in*c_out),
+                               nn.BatchNorm1d(2*c_in*c_out),
+                               nn.ReLU(),
+                               nn.Linear(2*c_in*c_out, c_in*c_out))
         nn.init.xavier_normal_(si_net[0].weight)
         nn.init.xavier_normal_(si_net[3].weight)
         nn.init.xavier_normal_(si_net[6].weight)
         si_nets.append(si_net)
-
+      
       self.si_nets = nn.ModuleList(si_nets)
 
   def forward(self, pc, f):
@@ -258,7 +270,7 @@ class GConv(nn.Module):
     for k in range(self.d_in+1):
       pc.graph.ndata[k] = f[k]
       for l in range(self.d_out+1):
-        pc.graph.edata[(k,l)] = self.wv_layers[k][l](pc)
+        pc.graph.edata[(k,l)] = self.wv_layers[k][l](pc, f)
     pc.graph.update_all(self.msg_func, self.rdc_func)
     f_out = dict()
     for l in range(self.d_out+1):
@@ -319,9 +331,8 @@ class GLinear(nn.Module):
     self.d_in = d_in
     self.c_in = c_in
     self.c_out = c_out
-    ws = nn.Parameter(torch.randn(d_in+1, c_out, c_in))
-    nn.init.xavier_normal_(ws)
-    self.ws = ws
+    self.ws = nn.Parameter(torch.randn(d_in+1, c_out, c_in))
+    nn.init.xavier_normal_(self.ws)
 
   def forward(self, f):
     out = dict()
@@ -330,7 +341,7 @@ class GLinear(nn.Module):
     return out
 
 class MultiHead(nn.Module):
-  def __init__(self, d_in=1, d_out=1, c_in=1, c_hid=None, c_out=1, heads = 1, attention=True, self_interaction=True):
+  def __init__(self, d_in=1, d_out=1, c_in=1, c_hid=None, c_out=1, heads=1, attention=True, self_interaction=True):
     super(MultiHead, self).__init__()
     self.d_in = d_in
     self.d_out = d_out
@@ -360,6 +371,7 @@ class MultiHead(nn.Module):
       f[l] = torch.cat(f_ls, dim=1)
     return f
 
+
 class GNonlinear(nn.Module):
   def __init__(self, d_in, c_in=1, c_out=1):
     super(GNonlinear, self).__init__()
@@ -376,238 +388,3 @@ class GNonlinear(nn.Module):
       norm = torch.norm(f[l],dim=2,keepdim=True)
       out[l] = self.relu(self.lns[l](norm[...,0])).unsqueeze(-1) * (f_l / norm)
     return out
-=======
-# -*- coding: utf-8 -*-
-"""se3-transformer.ipynb
-
-Automatically generated by Colaboratory.
-
-Original file is located at
-    https://colab.research.google.com/gist/bhedelius/ccee4e14859b0fa4f7cb6567a1a69777/se3-transformer.ipynb
-"""
-
-# Install libraries
-!pip install --pre dgl-cu101
-
-# Import libraries
-import torch
-from torch import nn
-import torch.nn.functional as F
-
-import dgl
-import dgl.function as fn
-from dgl import DGLGraph
-
-import numpy as np
-from scipy.special import sph_harm as sph_harm_func
-
-from numpy import sqrt
-from scipy.special import factorial
-
-
-def clebsch_gordan(j1,j2,m1,m2,J,M=None):
-  ''' Using equation from Wikipedia:
-  https://en.wikipedia.org/wiki/Table_of_Clebsch–Gordan_coefficients.
-
-  The equation isn't numerically stable and not fast (speed isn't too important)
-  TODO: Possibly find more stable implementation'''
-  if M is None:
-    M=m1+m2
-  if M<0:
-    return (-1)**(J-j1-j2)*clebsch_gordan(j1,j2,-m1,-m2,J,-M)
-  if j1<j2:
-    return (-1)**(J-j1-j2)*clebsch_gordan(j2,j1,m2,m1,J,M)
-  if not M==m1+m2:
-    return 0
-  A = sqrt((2*J+1)*factorial(J+j1-j2)*factorial(J-j1+j2)*factorial(j1+j2-J)/factorial(j1+j2+J+1))
-  B = sqrt(factorial(J+M)*factorial(J-M)*factorial(j1-m1)*factorial(j1+m1)*factorial(j2-m2)*factorial(j2+m2))
-  k_max = min([j1+j2-J,j1-m1,j2+m2])
-  k_min = max([0,-(J-j2+m1),-(J-j1-m2)])
-  C = 0
-  for k in range(int(k_min), int(k_max)+1):
-    C += (-1)**k/(factorial(k)*factorial(j1+j2-J-k)*factorial(j1-m1-k)*factorial(j2+m2-k)*factorial(J-j2+m1+k)*factorial(J-j1-m2+k))
-  return A*B*C
-
-def clebsch_gordan_mat(j1,j2,J,m):
-  mat = torch.zeros((int(2*j1+1),int(2*j2+1)))
-  for i, m1 in enumerate(torch.arange(-j1, j1+1)):
-    for j, m2 in enumerate(torch.arange(-j2, j2+1)):
-      mat[i,j] = clebsch_gordan(j1,j2,m1,m2,J,m)
-  return mat
-
-def clebsch_gordan_mats(j1,j2,J):
-  mats = torch.zeros(2*J+1,2*j1+1,2*j2+1)
-  for x, m in enumerate(torch.arange(-J, J+1)):
-    for y, m1 in enumerate(torch.arange(-j1, j1+1)):
-      for z, m2 in enumerate(torch.arange(-j2, j2+1)):
-        mats[x,y,z] = clebsch_gordan(j1,j2,m1,m2,J)
-  return mats
-
-clebsch_gordan(1/2,1/2,-1/2,1/2,1,0)
-
-class PointCloud:
-  '''Represents a point cloud in R^3. This class calculates and stores relevant
-  information such as the vectors, distances, directions, and spherical
-  harmonics of the vectors.'''
-  # new_g, new_ntypes, new_etypes, new_nframes, new_eframes
-  def __init__(self, pos, cutoff=8.0):
-    edges = self._find_edges_(pos, cutoff)
-    self.graph = dgl.graph(edges)
-    self.graph.ndata['pos'] = pos
-    self._calc_edge_info_()
-    self.sph_harm = dict()
-    self.wj = dict()
-
-  def _find_edges_(self, pos, cutoff):
-    # Use positions to create graph. Need to improve! Currently O(n^2)
-    self.vec_mat = pos[:,None,:]-pos[None,:,:]
-    self.dist_mat = torch.sqrt(torch.sum((self.vec_mat)**2,axis=-1))
-    u = []
-    v = []
-    for j in range(len(pos)):
-      for i in range(j):
-        if self.dist_mat[i,j] < cutoff:
-          u.append(i)
-          v.append(j)
-    u, v = torch.tensor(u+v), torch.tensor(v+u)
-    return (u,v)
-
-  def _calc_edge_info_(self):
-    # Calculate and store position and angle information
-    u,v = self.graph.edges()[0], self.graph.edges()[1]
-    vec = self.vec_mat[u,v]
-    self.graph.edata['vec'] = vec
-    dist = self.dist_mat[u,v]
-    self.graph.edata['dist'] = dist
-    dir = vec/dist[:,None]
-    self.graph.edata['dir'] = dir
-    self.graph.edata['theta'] = torch.atan2(dir[:,1], dir[:,0])
-    self.graph.edata['phi'] = torch.arccos(dir[:,2])
-
-  def get_sph_harm(self, J):
-    # Returns spherical harmonic of order J.
-    if not J in self.sph_harm.keys():
-      m = torch.arange(-J,J+1)
-      theta = self.graph.edata['theta']
-      phi = self.graph.edata['phi']
-      self.sph_harm[J] = torch.real(sph_harm_func(m[None,:], J, theta[:,None], phi[:,None])).double()
-    return self.sph_harm[J]
-    
-  def get_wj(self, l, k):
-    # This needs to be improved
-    if not (l,k) in self.wj.keys():
-      wj = torch.zeros(k+l-abs(k-l)+1, self.graph.number_of_edges(), 2*l+1, 2*k+1)
-      for i, J in enumerate(range(abs(k-l), k+l+1)):
-        sh = self.get_sph_harm(J)
-        cg = clebsch_gordan_mats(l,k,J).double()
-        wj[i] = torch.einsum("em,mlk->elk",sh, cg)
-      self.wj[(l,k)] = wj.transpose(0,1).clone()
-    return self.wj[(l,k)]
-
-class WLayer(nn.Module):
-  def __init__(self, k, l, channels=1):
-    super(WLayer, self).__init__()
-    self.k = k
-    self.l = l
-    self.channels = channels
-
-    r_size = (channels)*(k+l-abs(k-l)+1)*(2*l+1)*(2*k+1)
-
-    self.radial = nn.Sequential(nn.Linear(1,5), nn.ReLU(),
-                                nn.Linear(5,r_size))
-
-  def forward(self, pc):
-    wj = pc.get_wj(self.l, self.k)
-    R = self.radial(pc.graph.edata['dist'][:,None])
-    R = R.reshape((-1,
-                   self.channels,
-                   self.k+self.l-abs(self.k-self.l)+1,
-                   2*self.l+1,
-                   2*self.k+1))
-    w = torch.einsum('ecjlk,ejlk->eclk',R,wj)
-    return w
-
-class TFNLayer(nn.Module):
-  def __init__(self, k, l, channels=1):
-    super(TFNLayer, self).__init__()
-    self.k = k
-    self.l = l
-    self.channels = channels
-    self.wlayer = WLayer(k, l, channels)
-    #self.self_int = nn.linear
-
-  def forward(self, pc, feat):
-    with pc.graph.local_scope():
-      pc.graph.ndata['feat'] = feat
-      pc.graph.edata['w'] = self.wlayer(pc)
-      pc.graph.update_all(self.message_func, self.reduce_func)
-      return pc.graph.ndata['f']
-
-  def message_func(self, edges):
-    print(edges.data['w'].size(), edges.src['feat'].size())
-    return {'m': torch.einsum('eclk,eck->ecl',edges.data['w'],edges.src['feat'])}
-
-  def reduce_func(self,nodes):
-     return {'f': torch.sum(nodes.mailbox['m'], dim=1)}
-
-class TransLayer(nn.Module):
-  def __init__(self, k, l, channels=1):
-    super(TransLayer, self).__init__()
-    self.k = k
-    self.l = l
-    self.channels = channels
-
-    self.wklayer = WLayer(k, l, channels)
-    self.wvlayer = WLayer(k, l, channels)
-
-  def forward(self, pc, feat):
-    with pc.graph.local_scope():
-      pc.graph.ndata['feat'] = feat
-      pc.graph.edata['wk'] = self.wklayer(pc)
-      pc.graph.edata['wv'] = self.wvlayer(pc)
-      pc.graph.ndata['q'] = torch.sum(feat,-1)
-      pc.graph.update_all(self.attn_message, self.attn_reduce)
-      pc.graph.update_all(self.message_func, self.reduce_func)
-      return pc.graph.ndata['f']
-
-  def attn_message(self, edges):
-    q = edges.dst['q']
-    wk = edges.data['wk']
-    feat = edges.src['feat']
-    k = torch.sum(torch.einsum('eclk,eck->ecl',wk,feat),-1)
-    exp = torch.exp(
-      torch.einsum('ec,ec->ec',q,k))
-    edges.data['exp'] = exp
-    return {'exp': exp}
-
-  def attn_reduce(self, nodes):
-    # does sum over j'
-    return {'s': torch.sum(nodes.mailbox['exp'], dim=1)}
-
-  def message_func(self, edges):
-    attn = edges.data['exp']/edges.dst['s']
-    wv = edges.data['wv']
-    feat = edges.src['feat']
-
-    c = torch.einsum('ec,eclk,eck->ecl',attn,wv,feat)
-    si = edges.dst['feat']
-    return {'c': c+si}
-
-  def reduce_func(self,nodes):
-     return {'f': torch.sum(nodes.mailbox['c'], dim=1)}
-
-# Create a test graph
-num_pts = 10
-pos = torch.rand(num_pts,3)
-pc = PointCloud(pos)
-
-feat = torch.rand(num_pts,2,1)
-
-# TFN layer
-layer1 = TFNLayer(0,1,channels=2)
-layer1(pc, feat)
-
-# Transformer layer
-layer = TransLayer(0,1,channels=2)
-layer(pc, feat).size()
->>>>>>> be071eec8b0bb0dd819464cb4f5c82ddb0c8ac8b
