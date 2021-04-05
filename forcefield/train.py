4bab8f7bc86d95831e656eea11356dbccb6c30cf
Bryce Hedelius <50183652+bhedelius@users.noreply.github.com>
2021-04-05 12:22:26 -0600

    Update train.py

--- forcefield/train.py
+++ forcefield/train.py
@@ -12,75 +12,82 @@ from model import Model
 from anidataset import *
 
 import pickle as pkl
-
-def load(fname): 
-        with open(fname, 'rb') as handle:
-                b = pkl.load(handle)    
-        return b                       
-                    
+                 
 
 try:
-	print(type(dataset))
+    print(type(dataset))
 except:
-	print('Loading dataset')
-	dataset = AniDataset() 
+    print('Loading dataset')
+    dataset = AniDataset() 
 
 
 torch.cuda.empty_cache()
 
-#Let's see if this trains!
-losses = []
 
 dev = 'cuda:0'
-
+BATCH_SIZE = 50
 EPOCHS = 100
-
+energy_weight = 1e-5
 #setup the model
-model = Model()
-optimizer = torch.optim.Adam(model.parameters(), lr=1e-4, eps=1e-4)
-
-
 model = Model().to(dev)
+optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
 objective = nn.MSELoss()
-
-BATCH_SIZE = 200
 
 def train(i):
     graphs = []
     for idx in order[i:min(i+BATCH_SIZE,len(dataset))]:
-        pos, species, forces = dataset[idx] 
+        pos, species, forces, energy = dataset[idx] 
         pc = PointCloud(pos=pos, cutoff = 1000)
         graph = pc.graph
         graph.ndata[0] = species.unsqueeze(-1).float()
         graph.ndata['forces'] = forces
+        graph.ndata['energy'] = torch.ones(graph.num_nodes())*energy
         graphs.append(graph)
-    graph = dgl.batch(graphs)
-    pc = PointCloud(graph=graph)
-    pc.to(dev)
-    #build the point graph
+    batch = dgl.batch(graphs)
+    pc = PointCloud(graph=batch).to(dev)
     f = {0: pc.graph.ndata[0]}
+
+    optimizer.zero_grad()
     y_hat = model(pc, f)
-    loss = objective(y_hat[1][:,0,:], pc.graph.ndata['forces'])
+
+    energy_loss = 0
+    force_loss = 0
+    begin_node = 0
+    end_node = 0
+    for graph in graphs:
+        end_node += graph.num_nodes()
+        energy_out = torch.sum(y_hat[0][begin_node:end_node,0,0])
+        force_out = y_hat[1][begin_node:end_node,0,:]
+        energy_loss += objective(energy_out, pc.graph.ndata['energy'][begin_node])
+        force_loss  += objective(force_out,  pc.graph.ndata['forces'][begin_node:end_node])
+        begin_node = end_node
+    energy_loss /= len(graphs)
+    force_loss  /= len(graphs)
+    loss = energy_weight * energy_loss + force_loss
     loss.backward()
-    total_norm = torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
+
+    total_norm = torch.nn.utils.clip_grad_value_(model.parameters(), 1.0)
     if total_norm == total_norm:
         optimizer.step()
     else:
         print('gradient update skipped bc of Nans')
     losses.append(loss.item())
-    return loss.item()
+    return energy_loss.item(), force_loss.item()
 
+torch.autograd.set_detect_anomaly(True)
+losses = []
 
 for EPOCH in range(EPOCHS):
     order = torch.randperm(len(dataset))
     for i in range(len(dataset)//BATCH_SIZE):
-        print(i, train(i))
+        print(EPOCH, i, train(i))
         if i % 1000 == 0:
-            torch.save({
-            'epoch': EPOCH,
-            'model': model,
-            'optimizer_state_dict': optimizer.state_dict(),
-            'loss': losses}, './models/'+str(EPOCH)+'_'+str(i))
+            torch.save({'model': model,
+                        'optimizer_state_dict': optimizer.state_dict()},
+                       f'./models/{EPOCH}_{i}.pkl)')
+            np.save(f'./models/{EPOCH}_{i}.npy',np.array(losses))
+            losses = []
+
 
 ##### HOW TO LOAD A SAVED MODEL
 '''
@@ -101,7 +108,3 @@ model.train()
 #import plotext as plt
 #plt.plot(range(len(losses)), losses)
 #plt.show()
-
-
-
-
